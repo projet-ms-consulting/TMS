167ebf7d9fad7821b8b3be9265978a8809f09096
Fugann <95033152+Fugann@users.noreply.github.com>
2024-04-16 21:47:07 +0200

    05-Ajout de la page d'accueil du panel admin

---
+++ src/Controller/admin/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin', name: 'admin_')]
+class MainController extends AbstractController
+{
+    #[Route('', name: 'main')]
+    public function index(): Response
+    {
+        return $this->render('admin/main/index.html.twig');
+    }
+}
