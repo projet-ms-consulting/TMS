60aef7148a2307da06ccd178da318a1cf1a2626d
ccosco <charlotte.cosco@hotmail.fr>
2024-07-02 16:22:11 +0200

    Création stagiaire

---
+++ src/Controller/admin/PersonController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Files;
+use App\Entity\Person;
+use App\Form\PersonType;
+use App\Repository\PersonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/person')]
+class PersonController extends AbstractController
+{
+    #[Route('/', name: 'app_person_index', methods: ['GET'])]
+    public function index(PersonRepository $personRepository): Response
+    {
+        return $this->render('person/index.html.twig', [
+            'people' => $personRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_person_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $person = new Person();
+        $personForm = $this->createForm(PersonType::class, $person);
+        $personForm->handleRequest($request);
+
+        if ($personForm->isSubmitted() && $personForm->isValid()) {
+
+            // Upload CV
+            $cvFile = $personForm->get('cv')->getData();
+            if ($cvFile) {
+                $cvFilename ='CV.' . $person->getFirstName().'-'.$person->getLastName() . '.'.$cvFile->guessExtension();
+                $cvFile->move($this->getParameter('kernel.project_dir') . '/public/CV', $cvFilename );
+
+                $file = new Files();
+                $file->setLabel('CV')->setFile($cvFilename)->setCreatedAt(new \DateTimeImmutable())->setPerson($person);
+
+                $entityManager->persist($file);
+                $entityManager->persist($file->getPerson());
+                $entityManager->flush();
+            }
+
+            // Upload Lettre de motivation
+            $lmFile = $personForm->get('coverLetter')->getData();
+            if ($lmFile) {
+                $lmFilename ='LM.' . $person->getFirstName().'-'.$person->getLastName() . '.'.$lmFile->guessExtension();
+                $lmFile->move($this->getParameter('kernel.project_dir') . '/public/LM', $lmFilename );
+
+                $file = new Files();
+                $file->setLabel('LM')->setFile($lmFilename)->setCreatedAt(new \DateTimeImmutable())->setPerson($person);
+
+                $entityManager->persist($file);
+                $entityManager->persist($file->getPerson());
+                $entityManager->flush();
+            }
+
+            $csFile = $personForm->get('internshipAgreement')->getData();
+            if ($csFile) {
+                $csFilename ='CS.' . $person->getFirstName().'-'.$person->getLastName() . '.'.$csFile->guessExtension();
+                $csHash = hash('sha256', $csFilename);
+                $csHashFile =$csHash . '.' . $csFile->guessExtension();
+                $csFile->move($this->getParameter('kernel.project_dir') . '/public/CS', $csHashFile );
+
+                $file = new Files();
+                $file->setLabel('CS')->setFile($csHashFile)->setCreatedAt(new \DateTimeImmutable())->setPerson($person);
+
+                $entityManager->persist($file);
+                $entityManager->persist($file->getPerson());
+                $entityManager->flush();
+            }
+
+            $entityManager->persist($person);
+            $entityManager->flush(); // Upload Convention de stage
+
+
+            return $this->redirectToRoute('app_person_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('person/new.html.twig', [
+            'person' => $person,
+            'personForm' => $personForm,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_person_show', methods: ['GET'])]
+    public function show(Person $person): Response
+    {
+        return $this->render('person/show.html.twig', [
+            'person' => $person,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_person_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Person $person, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PersonType::class, $person);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_person_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('person/edit.html.twig', [
+            'person' => $person,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_person_delete', methods: ['POST'])]
+    public function delete(Request $request, Person $person, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$person->getId(), $request->getPayload()->get('_token'))) {
+            $entityManager->remove($person);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_person_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PersonType.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Person;
+use App\Entity\School;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName', null, [
+                'label' => 'Nom : ',
+            ])
+            ->add('firstName', null, [
+                'label' => 'Prénom : ',
+            ])
+            ->add('startInternship', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date début de stage : ',
+            ])
+            ->add('endInternship', null, [
+                'widget' => 'single_text',
+                'label' => 'Date fin de stage : ',
+            ])
+            ->add('school', EntityType::class, [
+                'class' => School::class,
+                'label' => 'Ecole : ',
+                'choice_label' => 'school.name',
+            ])
+            ->add('cv', FileType::class,[
+                'label' => 'Ajouter un CV  : ',
+                'mapped'=> false,
+                'required' => false,
+//                'constraints'=> [
+//                    new File([
+//                        'mimeTypes' => [
+//                            'application/pdf',
+//                            'application/x-pdf',
+//                        ]
+//                    ])
+//                ]
+            ])
+            ->add('coverLetter', FileType::class,[
+                'label' => 'Ajouter une lettre de motivation : ',
+                'mapped'=> false,
+                'required' => false,
+//                'constraints'=> [
+//                    new File([
+//                        'mimeTypes' => [
+//                            'application/pdf',
+//                            'application/x-pdf',
+//                        ]
+//                    ])
+//                ]
+            ])
+            ->add('internshipAgreement', FileType::class,[
+                'label' => 'Ajouter une convention de stage : ',
+                'mapped'=> false,
+                'required' => false,
+//                'constraints'=> [
+//                    new File([
+//                        'mimeTypes' => [
+//                            'application/pdf',
+//                            'application/x-pdf',
+//                        ]
+//                    ])
+//                ]
+            ])
+            ->add('createdAt', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Créer le : ',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Person::class,
+        ]);
+    }
+}
