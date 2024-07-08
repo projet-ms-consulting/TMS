2e90b77cc5d39aef5482ef8fc42ec412c6c91f88
Jenny <158514198+Jenny-nct@users.noreply.github.com>
2024-07-08 12:11:41 +0200

    Css
    css general + array

--- assets/JavaScript/navbar.js
+++ assets/JavaScript/navbar.js
@@ -1,10 +1,10 @@
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.navbar-toggle');
-    const menu = document.querySelector('.navbar-menu');
-
-    toggleButton.addEventListener('click', function () {
-        menu.classList.toggle('active');
-    });
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     const toggleButton = document.querySelector('.navbar-toggle');
+//     const menu = document.querySelector('.navbar-menu');
+//
+//     toggleButton.addEventListener('click', function () {
+//         menu.classList.toggle('active');
+//     });
+// });
