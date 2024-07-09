5105198324517ef8d1df9b6fd0d3332da2dc701c
CharlotteAR <charlotte.cosco@hotmail.fr>
2024-07-09 14:09:44 +0200

    User + Rôles
    En cours

---
+++ src/Form/UserType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Person;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormTypeInterface;
+
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email : '
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe : ',
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Role : ',
+                'multiple' => true,
+                'expanded' => false,
+                'choices' => [
+                    'Super Admin' => 'ROLE_SUPER_ADMIN',
+                    'Manager' => 'ROLE_ADMIN',
+                    'Stagiaire' => 'ROLE_TRAINEE',
+                    'Responsable école' => 'ROLE_SCHOOL_SUPERVISOR',
+                    'Responsable de stage' => 'ROLE_COMPANY_INTERNSHIP',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'selected_person' => null,
+        ]);
+    }
+}
