b91bc325ad6222d2575d97f16831cb338e8fd2d3
Jenny <158514198+Jenny-nct@users.noreply.github.com>
2024-07-09 11:15:36 +0200

    Profil
    -Récupère les informations suivantes : email, rôle, date création, date modification
    -À modifier pour récupérer le nom/prénom/ lieux rattachés/Cv => en cours

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -4,14 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+       $userFromDatabase = $userRepository->find($user->getId());
+
+        $person = $userFromDatabase->getPerson();
+
         return $this->render('profil/index.html.twig', [
+            'user' => $userFromDatabase,
+            'person' => $person,
         ]);
     }
 }
---
+++ src/Entity/Profil.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfilRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProfilRepository::class)]
+class Profil
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
