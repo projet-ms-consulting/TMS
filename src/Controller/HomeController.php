ec673c9d560063b72511b8f9fdfbcb464cee8053
Fugann <95033152+Fugann@users.noreply.github.com>
2024-07-18 13:08:37 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Person;
 use App\Entity\User;
+use App\Repository\PersonRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -13,38 +14,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('', name: 'app_')]
 class HomeController extends AbstractController
 {
-    private $security;
-
-    private $entityManager;
-
-    public function __construct(Security $security, EntityManagerInterface $entityManager)
-    {
-        $this->security = $security;
-        $this->entityManager = $entityManager;
-    }
-
     #[Route('', name: 'accueil')]
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-        $user = $this->security->getUser();
-        $person = $entityManager->getRepository(Person::class)->find(1);
-        return $this->render('home/index.html.twig', [
-            'user' => $user,
-            'person' => $person,
-        ]);
-    }
-
-    public function home(): Response
+    public function index(PersonRepository $personRepository, Security $security): Response
     {
         $user = $this->getUser();
-        if ($user instanceof User) {
-            $person = $user->getPerson();
-        } else {
-            $person = null;
-        }
-
-        return $this->render('base.html.twig', [
-            'user' => $user,
+        $person = $personRepository->find($user->getPerson()->getId());
+        return $this->render('home/index.html.twig', [
             'person' => $person,
         ]);
     }
