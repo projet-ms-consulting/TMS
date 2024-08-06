6377e24b554b68e761ddc073756ce9de765a32fe
Shuuhei60 <95132453+Shuuhei60@users.noreply.github.com>
2024-08-06 17:10:19 +0200

    push avant prod

--- src/Form/SchoolEditType.php
+++ src/Form/SchoolEditType.php
@@ -29,7 +29,9 @@ class SchoolEditType extends AbstractType
             ])
 
             ->add('address', AddressType::class, [
-                'required' => false,
+                'data' => $options['data']->getAddress(),
+                'mapped' => false,
+                'label' => 'Adresse',
             ]);
     }
 
